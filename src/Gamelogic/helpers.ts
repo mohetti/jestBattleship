286a1fc1bb392fa6c3e8ccd9e2d3e72cb34bf922
mohetti <moritz.hettich@gmx.de>
2021-12-15 13:37:46 +0100

    short update of helpers.ts. possibleCoords needs extra care so the right coords are put into the array

--- src/Gamelogic/helpers.ts
+++ src/Gamelogic/helpers.ts
@@ -13,6 +13,21 @@ function getAxis() {
   return Math.floor(Math.random() * 2) === 0;
 }
 
+function possibleCoords(
+  startCoord: number[],
+  shipCells: number,
+  isHorizontal: boolean
+) {
+  let possible = [startCoord];
+  // vorsicht
+  if (isHorizontal) {
+    for (let i = startCoord[1]; i < shipCells - 1; i++) {
+      let coord = [startCoord[0], startCoord[1] + 1];
+      possible.push();
+    }
+  }
+}
+
 function finalShipPlacement(
   board: number[][],
   shipCells: number,
@@ -20,23 +35,35 @@ function finalShipPlacement(
 ) {
   if (isHorizontal === undefined) isHorizontal = getAxis();
 
-  // return values
+  // recursive outcome comese below and before boundary
   const boundary = 10 - shipCells;
   const inboundArr = board.filter((x) => {
     return isHorizontal ? x[1] <= boundary : x[0] <= boundary;
   });
+
+  let randomIndex = getRandomIndex(inboundArr.length);
+
+  let possible = possibleCoords(
+    inboundArr[randomIndex],
+    shipCells,
+    isHorizontal
+  );
+
+  // *** place ship randomly
+
+  // coordinate-tuples cant be the same or be out of bounce. so this doesnt need to be tested.
+  // but, coordinate-tuples could jump over a coordinate that is not in the array anymore, like: [0,2] [0,4]
+  // *** testing if a jump happened. If no: finalShipPlacement is finished. If jump happened, try again recursevly
 }
 
 export function randomFleet(board: number[][] = emptyBoardArray()) {
   const emptyBoard = board;
-  let randomIndex = getRandomIndex(board.length);
-  const tinyShip = board[randomIndex];
-  board.splice(randomIndex, 1);
+  let randomIndex = getRandomIndex(emptyBoard.length);
+  const tinyShip = emptyBoard[randomIndex];
+  emptyBoard.splice(randomIndex, 1);
 
   const smallShip = finalShipPlacement(board, 3);
-
-  // x.filter(x => x[1] !== smth) filtert später alle raus, die für zB bigShip sowieso nicht in Frage kommen
-  //
+  // find those coordinates and remove them from emptyBoard. Since they're unique, no deeper logic is needed
 
   return { bigShip: [], medShip: [], smallShip: [], tinyShip: [] };
 }
